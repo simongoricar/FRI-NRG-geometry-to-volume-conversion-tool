56538d779ed5c5ec036e2c405c734df63089915f
Simon Goričar <simon.peter.goricar@gmail.com>
2024-05-19 18:18:47 +0200

    Add first voxelizer version, start working on the visualization with bevy

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,57 @@
 use std::{path::Path, str::FromStr};
 
+use bevy::{
+    app::{App, PreUpdate}, asset::Handle, core_pipeline::core_3d::Camera3dBundle, ecs::{query::With, system::{Commands, Local, Query, ResMut}}, math::Vec3A, pbr::{CascadeShadowConfigBuilder, DirectionalLight, DirectionalLightBundle}, prelude::{AssetServer, Res, Resource, SceneBundle, Startup, Transform, Vec3}, render::{camera::{Camera, PerspectiveProjection}, mesh::Mesh, primitives::Sphere}, transform::components::GlobalTransform, DefaultPlugins
+};
 use clap::Parser;
 use easy_gltf::model::Triangle;
 use miette::{miette, Context, IntoDiagnostic, Result};
+use nalgebra::Vector3;
+use scene_viewer_plugin::SceneHandle;
+use tracing::info;
 use tracing_subscriber::EnvFilter;
+use voxelizer::VoxelGrid;
 
-use crate::{cli::CliArgs, logging::initialize_tracing, voxelizer::voxelize_triangles};
+use crate::{
+    camera_controller::{CameraController, CameraControllerPlugin}, cli::CliArgs, logging::initialize_tracing, scene_viewer_plugin::SceneViewerPlugin, voxelizer::{voxelize_triangles, Aabb}
+};
 
+// #![allow(clippy::type_complexity)]
+
+mod camera_controller;
 mod cli;
 mod logging;
 mod voxelizer;
+mod scene_viewer_plugin;
+
+
+fn load_gltf_scene_from_file<P>(gltf_file_path: P) -> Result<easy_gltf::Scene>
+where
+    P: AsRef<Path>,
+{
+    let gltf_scenes = easy_gltf::load(gltf_file_path)
+        .map_err(|error| miette!("Failed to load GLTF file: {error:?}"))?;
+
+    let Some(first_scene) = gltf_scenes.into_iter().next() else {
+        return Err(miette!("Provided GLTF file contains no scenes."));
+    };
+
+    Ok(first_scene)
+}
+
+fn collect_mesh_triangles(gltf_scene: &easy_gltf::Scene) -> Result<Vec<Triangle>> {
+    let mut collected_triangles: Vec<Triangle> = Vec::new();
+
+    for model in &gltf_scene.models {
+        let model_triangles: Vec<Triangle> = model
+            .triangles()
+            .map_err(|error| miette!("Failed to get triangles for model. Reason: {error:?}"))?;
+
+        collected_triangles.extend(model_triangles);
+    }
+
+    Ok(collected_triangles)
+}
 
 
 fn load_mesh_triangles_from_file<P>(gltf_file_path: P) -> Result<Vec<Triangle>>
@@ -58,20 +100,198 @@ fn main() -> Result<()> {
         "nrg-m2v",
     );
 
+    /*
+       let gltf_scene = load_gltf_scene_from_file(cli_args.input_file_path)
+           .wrap_err("Failed to load GLTF scene.")?;
 
-    let mesh_triangles = load_mesh_triangles_from_file(cli_args.input_file_path)
-        .wrap_err("Failed to load mesh and parse triangles from GLTF file.")?;
+       let mesh_triangles =
+           collect_mesh_triangles(&gltf_scene).wrap_err("Failed to collect triangles from scene.")?;
 
-    println!("Loaded {} triangles.", mesh_triangles.len());
+       println!("Loaded {} triangles.", mesh_triangles.len());
 
 
-    // TODO run through voxelizer
-    voxelize_triangles(mesh_triangles);
+       // TODO run through voxelizer
+       let voxel_grid = voxelize_triangles(
+           mesh_triangles,
+           Aabb {
+               min: Vector3::new(-3.0, -3.0, -3.0),
+               max: Vector3::new(3.0, 3.0, 3.0),
+           },
+           0.5,
+       );
+    */
+
+    println!("Voxelization complete, starting visualization.");
+
+
+
 
     // TODO export to BVP format
 
     // TODO visualize?
 
+
+    App::new()
+        .add_plugins((DefaultPlugins, CameraControllerPlugin, SceneViewerPlugin))
+        // .insert_resource(Scene { gltf_scene })
+        // .insert_resource(VoxelizedMesh { voxel_grid })
+        .add_systems(Startup, startup)
+        .add_systems(PreUpdate, setup_scene_after_load)    
+        .run();
+
+
     drop(logging_guard);
     Ok(())
+}
+
+
+#[derive(Resource)]
+pub struct Scene {
+    gltf_scene: easy_gltf::Scene,
+}
+
+#[derive(Resource)]
+pub struct VoxelizedMesh {
+    voxel_grid: VoxelGrid,
+}
+
+/*
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut projection = PerspectiveProjection::default();
+    projection.far = projection.far.max(20.0);
+
+    let camera_controller = CameraController::default();
+
+    info!("{}", camera_controller);
+
+    commands.spawn((
+        Camera3dBundle {
+            projection: projection.into(),
+            transform: Transform::from_xyz(4.0, 4.0, 4.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+            camera: Camera {
+                is_active: false,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        camera_controller,
+    ));
+
+    // commands.spawn(Camera3dBundle {
+    //     transform: Transform::from_xyz(4.0, 4.0, 4.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+    //     ..Default::default()
+    // });
+
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("mesh-sample-02/mesh-sample-02_simple-cube.glb#Scene0"),
+        ..Default::default()
+    });
+}
+*/
+
+fn parse_scene(scene_path: String) -> (String, usize) {
+    if scene_path.contains('#') {
+        let gltf_and_scene = scene_path.split('#').collect::<Vec<_>>();
+        if let Some((last, path)) = gltf_and_scene.split_last() {
+            if let Some(index) = last
+                .strip_prefix("Scene")
+                .and_then(|index| index.parse::<usize>().ok())
+            {
+                return (path.join("#"), index);
+            }
+        }
+    }
+    (scene_path, 0)
+}
+
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let (file_path, scene_index) = parse_scene("mesh-sample-01/mesh-sample-01_torus.glb".to_string());
+
+    commands.insert_resource(SceneHandle::new(asset_server.load(file_path), scene_index));
+} 
+
+
+fn setup_scene_after_load(
+    mut commands: Commands,
+    mut setup: Local<bool>,
+    mut scene_handle: ResMut<SceneHandle>,
+    meshes: Query<(&GlobalTransform, Option<&bevy::render::primitives::Aabb>), With<Handle<Mesh>>>,
+) {
+    if scene_handle.is_loaded && !*setup {
+        println!("Scene loaded, running setup!");
+
+        *setup = true;
+        // Find an approximate bounding box of the scene from its meshes
+        if meshes.iter().any(|(_, maybe_aabb)| maybe_aabb.is_none()) {
+            return;
+        }
+
+        let mut min = Vec3A::splat(f32::MAX);
+        let mut max = Vec3A::splat(f32::MIN);
+        for (transform, maybe_aabb) in &meshes {
+            let aabb = maybe_aabb.unwrap();
+            // If the Aabb had not been rotated, applying the non-uniform scale would produce the
+            // correct bounds. However, it could very well be rotated and so we first convert to
+            // a Sphere, and then back to an Aabb to find the conservative min and max points.
+            let sphere = Sphere {
+                center: Vec3A::from(transform.transform_point(Vec3::from(aabb.center))),
+                radius: transform.radius_vec3a(aabb.half_extents),
+            };
+            let aabb = bevy::render::primitives::Aabb::from(sphere);
+            min = min.min(aabb.min());
+            max = max.max(aabb.max());
+        }
+
+        let size = (max - min).length();
+        let aabb = bevy::render::primitives::Aabb::from_min_max(Vec3::from(min), Vec3::from(max));
+
+        info!("Spawning a controllable 3D perspective camera");
+        let mut projection = PerspectiveProjection::default();
+        projection.far = projection.far.max(size * 10.0);
+
+        let camera_controller = CameraController::default();
+
+        // Display the controls of the scene viewer
+        info!("{}", camera_controller);
+        info!("{}", *scene_handle);
+
+        commands.spawn((
+            Camera3dBundle {
+                projection: projection.into(),
+                transform: Transform::from_translation(
+                    Vec3::from(aabb.center) + size * Vec3::new(0.5, 0.25, 0.5),
+                )
+                .looking_at(Vec3::from(aabb.center), Vec3::Y),
+                camera: Camera {
+                    is_active: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            camera_controller,
+        ));
+
+        // Spawn a default light if the scene does not have one
+        if !scene_handle.has_light {
+            info!("Spawning a directional light");
+            commands.spawn(DirectionalLightBundle {
+                transform: Transform::from_xyz(4.0, 4.0, 4.0).looking_at(Vec3::ZERO, Vec3::Y),
+                ..Default::default()
+            });
+
+            scene_handle.has_light = true;
+        }
+    }
 }
--- src/voxelizer.rs
+++ src/voxelizer.rs
@@ -1,6 +1,195 @@
-use easy_gltf::model::Triangle;
+use std::ops::Div;
 
-pub fn voxelize_triangles(triangles: Vec<Triangle>) -> ! {
+use easy_gltf::model::{Triangle, Vertex};
+use nalgebra::{Point3, Vector3};
+use parry3d::query::details::intersection_test_aabb_triangle;
+
+
+#[derive(Clone, Debug)]
+pub struct Voxel {
+    grid_index: Vector3<usize>,
+    is_filled: bool,
+}
+
+impl Voxel {
+    pub fn get_center_coordinate_in_world_space(
+        &self,
+        starting_voxel_grid_point: Vector3<f32>,
+        voxel_half_size: f32,
+    ) -> Vector3<f32> {
+        let first_voxel_center = starting_voxel_grid_point.add_scalar(voxel_half_size);
+
+        Vector3::new(
+            first_voxel_center.x + (voxel_half_size * 2.0 * self.grid_index.x as f32),
+            first_voxel_center.y + (voxel_half_size * 2.0 * self.grid_index.y as f32),
+            first_voxel_center.z + (voxel_half_size * 2.0 * self.grid_index.z as f32),
+        )
+    }
+}
+
+
+pub struct VoxelGrid {
+    starting_point: Vector3<f32>,
+    voxel_half_size: f32,
+
+    x_length: usize,
+    y_length: usize,
+    z_length: usize,
+
+    /// Indexed by x, y, and z, flattened out as a single Vec.
+    grid: Vec<Voxel>,
+}
+
+pub struct Aabb {
+    pub min: Vector3<f32>,
+    pub max: Vector3<f32>,
+}
+
+impl Aabb {
+    #[inline]
+    pub fn center(&self) -> Vector3<f32> {
+        Vector3::new(
+            (self.max.x + self.min.x) / 2.0,
+            (self.max.y + self.min.y) / 2.0,
+            (self.max.z + self.min.z) / 2.0,
+        )
+    }
+
+    #[inline]
+    pub fn half_reach(&self) -> Vector3<f32> {
+        Vector3::new(
+            (self.max.x - self.min.x) / 2.0,
+            (self.max.y - self.min.y) / 2.0,
+            (self.max.z - self.min.z) / 2.0,
+        )
+    }
+}
+
+
+
+impl VoxelGrid {
+    /// `starting_point` is the bounding box edge of the lowest-x lowest-y lowest-z voxel
+    /// (meaning the first voxel center is at `starting_point + voxel_half_size`).
+    pub fn new(
+        starting_point: Vector3<f32>,
+        voxel_half_size: f32,
+        x_size: usize,
+        y_size: usize,
+        z_size: usize,
+    ) -> Self {
+        let mut grid = Vec::with_capacity(x_size * y_size * z_size);
+
+        for z_index in 0..z_size {
+            for y_index in 0..y_size {
+                for x_index in 0..x_size {
+                    let voxel = Voxel {
+                        grid_index: Vector3::new(x_index, y_index, z_index),
+                        is_filled: false,
+                    };
+
+                    grid.push(voxel);
+                }
+            }
+        }
+
+        Self {
+            starting_point,
+            voxel_half_size,
+            x_length: x_size,
+            y_length: y_size,
+            z_length: z_size,
+            grid,
+        }
+    }
+
+    pub fn voxel_mut_by_xyz_index_unchecked(&mut self, x: usize, y: usize, z: usize) -> &mut Voxel {
+        self.grid
+            .get_mut(x + (y * self.x_length) + (z * self.y_length * self.x_length))
+            .unwrap()
+    }
+
+    pub fn overlapping_voxels_mut_by_bounding_box_in_world_spacee(
+        &mut self,
+        axis_aligned_bounding_box: Aabb,
+    ) -> Vec<&mut Voxel> {
+        todo!();
+    }
+}
+
+
+/*
+fn check_box_triangle_collision(triangle: &[Vertex; 3], aabb: &Aabb) -> bool {
+    let mut vertex_first = Vector3::new(
+        triangle[0].position.x,
+        triangle[0].position.y,
+        triangle[0].position.z,
+    );
+
+    let mut vertex_second = Vector3::new(
+        triangle[1].position.x,
+        triangle[1].position.y,
+        triangle[1].position.z,
+    );
+
+    let mut vertex_third = Vector3::new(
+        triangle[2].position.x,
+        triangle[2].position.y,
+        triangle[2].position.z,
+    );
+
+
+    let aabb_center = aabb.center();
+    let aabb_half_reach = aabb.half_reach();
+
+    // Move the vertices to the origin.
+    vertex_first -= aabb_center;
+    vertex_second -= aabb_center;
+    vertex_third -= aabb_center;
+
+
+    // Compute edge vectors for the triangle.
+    let edge_one = vertex_second - vertex_first;
+    let edge_two = vertex_third - vertex_second;
+    let edge_three = vertex_first - vertex_third;
+
+
+    let aabb_normal_one = Vector3::new(1.0, 0.0, 0.0);
+    let aabb_normal_two = Vector3::new(0.0, 1.0, 0.0);
+    let aabb_normal_three = Vector3::new(0.0, 0.0, 1.0);
+
+
+    // Construct 9 axis for the SAT.
+    let axis_edge_one_normal_one = aabb_normal_one.cross(&edge_one);
+    let axis_edge_two_normal_one = aabb_normal_one.cross(&edge_two);
+    let axis_edge_three_normal_one = aabb_normal_one.cross(&edge_three);
+
+    let axis_edge_one_normal_two = aabb_normal_two.cross(&edge_one);
+    let axis_edge_two_normal_two = aabb_normal_two.cross(&edge_two);
+    let axis_edge_three_normal_two = aabb_normal_two.cross(&edge_three);
+
+    let axis_edge_one_normal_three = aabb_normal_three.cross(&edge_one);
+    let axis_edge_two_normal_three = aabb_normal_three.cross(&edge_two);
+    let axis_edge_three_normal_three = aabb_normal_three.cross(&edge_three);
+
+
+    // Test `axis_edge_one_normal_one` axis.
+
+    // Project all three vertices onto the separating axis.
+    let projection_one = vertex_first.dot(&axis_edge_one_normal_one);
+    let projection_two = vertex_second.dot(&axis_edge_one_normal_one);
+    let projection_three = vertex_third.dot(&axis_edge_one_normal_one);
+
+    todo!();
+} */
+
+
+
+
+pub fn voxelize_triangles(
+    triangles: Vec<Triangle>,
+    voxelization_bounds: Aabb,
+    voxel_size: f32,
+) -> VoxelGrid {
     // TODO
 
     //! ## Pseudocode (creates a hollow volume)
@@ -29,6 +218,151 @@ pub fn voxelize_triangles(triangles: Vec<Triangle>) -> ! {
     //! ```
     //!
     //!
+
+
+    let mut voxel_grid = {
+        let num_voxels_on_x =
+            (voxelization_bounds.max.x - voxelization_bounds.min.x).div(voxel_size) as usize;
+        let num_voxels_on_y =
+            (voxelization_bounds.max.y - voxelization_bounds.min.y).div(voxel_size) as usize;
+        let num_voxels_on_z =
+            (voxelization_bounds.max.z - voxelization_bounds.min.z).div(voxel_size) as usize;
+
+        VoxelGrid::new(
+            voxelization_bounds.min,
+            voxel_size / 2.0,
+            num_voxels_on_x,
+            num_voxels_on_y,
+            num_voxels_on_z,
+        )
+    };
+
+    for triangle in triangles {
+        let triangle_aabb = {
+            let minimum_triangle_x = triangle[0]
+                .position
+                .x
+                .min(triangle[1].position.x)
+                .min(triangle[2].position.x);
+
+            let minimum_triangle_y = triangle[0]
+                .position
+                .y
+                .min(triangle[1].position.y)
+                .min(triangle[2].position.y);
+
+            let minimum_triangle_z = triangle[0]
+                .position
+                .z
+                .min(triangle[1].position.z)
+                .min(triangle[2].position.z);
+
+
+            let maximum_triangle_x = triangle[0]
+                .position
+                .x
+                .max(triangle[1].position.x)
+                .max(triangle[2].position.x);
+
+            let maximum_triangle_y = triangle[0]
+                .position
+                .y
+                .max(triangle[1].position.y)
+                .max(triangle[2].position.y);
+
+            let maximum_triangle_z = triangle[0]
+                .position
+                .z
+                .max(triangle[1].position.z)
+                .max(triangle[2].position.z);
+
+
+            Aabb {
+                min: Vector3::new(
+                    minimum_triangle_x,
+                    minimum_triangle_y,
+                    minimum_triangle_z,
+                ),
+                max: Vector3::new(
+                    maximum_triangle_x,
+                    maximum_triangle_y,
+                    maximum_triangle_z,
+                ),
+            }
+        };
+
+        let (index_x_start, index_y_start, index_z_start) = {
+            let starting_x_index =
+                (triangle_aabb.min.x - voxel_grid.starting_point.x).div(voxel_size) as usize;
+            let starting_y_index =
+                (triangle_aabb.min.y - voxel_grid.starting_point.y).div(voxel_size) as usize;
+            let starting_z_index =
+                (triangle_aabb.min.z - voxel_grid.starting_point.z).div(voxel_size) as usize;
+
+            (
+                starting_x_index,
+                starting_y_index,
+                starting_z_index,
+            )
+        };
+
+        let (index_x_num, index_y_num, index_z_num) = {
+            let x_index_length =
+                (triangle_aabb.max.x - triangle_aabb.min.x).div(voxel_size) as usize + 2;
+            let y_index_length =
+                (triangle_aabb.max.y - triangle_aabb.min.y).div(voxel_size) as usize + 2;
+            let z_index_length =
+                (triangle_aabb.max.z - triangle_aabb.min.z).div(voxel_size) as usize + 2;
+
+            (x_index_length, y_index_length, z_index_length)
+        };
+
+
+        for grid_index_x in index_x_start..(index_x_start + index_x_num) {
+            for grid_index_y in index_y_start..(index_y_start + index_y_num) {
+                for grid_index_z in index_z_start..(index_z_start + index_z_num) {
+                    let intersects = intersection_test_aabb_triangle(
+                        &parry3d::bounding_volume::Aabb::new(
+                            Point3::from(triangle_aabb.min),
+                            Point3::from(triangle_aabb.max),
+                        ),
+                        &parry3d::shape::Triangle::new(
+                            Point3::new(
+                                triangle[0].position.x,
+                                triangle[0].position.y,
+                                triangle[0].position.z,
+                            ),
+                            Point3::new(
+                                triangle[1].position.x,
+                                triangle[1].position.y,
+                                triangle[1].position.z,
+                            ),
+                            Point3::new(
+                                triangle[2].position.x,
+                                triangle[2].position.y,
+                                triangle[2].position.z,
+                            ),
+                        ),
+                    );
+
+                    if intersects {
+                        let intersected_voxel = voxel_grid.voxel_mut_by_xyz_index_unchecked(
+                            grid_index_x,
+                            grid_index_y,
+                            grid_index_z,
+                        );
+
+                        intersected_voxel.is_filled = true;
+                    }
+
+                    // todo!("get mutable Voxel and its center point");
+                    // todo!("check for triangle-AABB intersection");
+                    // todo!("if intersected, fill the voxel");
+                }
+            }
+        }
+    }
+
 
     todo!();
 }
